86a359ba36e344f042c778eae7ed4c1449d1db21
Soufiane Dahani <sodahani@c4r7p9.1337.ma>
2025-02-08 20:56:43 +0100

    i have data race in my code

--- Simulate.c
+++ Simulate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Simulate.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sodahani <sodahani@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: sodahani <sodahani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:34:41 by sodahani          #+#    #+#             */
-/*   Updated: 2025/02/04 22:08:02 by sodahani         ###   ########.fr       */
+/*   Updated: 2025/02/08 20:55:15 by sodahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,16 +60,8 @@ void	pick_forks(t_philo *philo)
 	right_fork = philo->id % philo->data->num_philos;
 	if (philo->id % 2 == 0)
 		usleep(200);
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(&philo->data->forks[right_fork]);
-		pthread_mutex_lock(&philo->data->forks[left_fork]);
-	}
-	else
-	{
-		pthread_mutex_lock(&philo->data->forks[left_fork]);
-		pthread_mutex_lock(&philo->data->forks[right_fork]);
-	}
+	pthread_mutex_lock(&philo->data->forks[left_fork]);
+	pthread_mutex_lock(&philo->data->forks[right_fork]);
 }
 
 void	release_forks(t_philo *philo)
@@ -81,13 +73,13 @@ void	release_forks(t_philo *philo)
 	right_fork = philo->id % philo->data->num_philos;
 	if (philo->id % 2 == 0)
 	{
-		pthread_mutex_unlock(&philo->data->forks[left_fork]);
 		pthread_mutex_unlock(&philo->data->forks[right_fork]);
+		pthread_mutex_unlock(&philo->data->forks[left_fork]);
 	}
 	else
 	{
-		pthread_mutex_unlock(&philo->data->forks[right_fork]);
 		pthread_mutex_unlock(&philo->data->forks[left_fork]);
+		pthread_mutex_unlock(&philo->data->forks[right_fork]);
 	}
 }
 
