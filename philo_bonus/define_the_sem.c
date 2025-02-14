8189341c1e7d562a6c52d1c87f698f2dc13c75ff
Soufiane Dahani <sodahani@c4r8p9.1337.ma>
2025-02-14 18:18:40 +0100

    handle some leaks 4

--- philo_bonus/define_the_sem.c
+++ philo_bonus/define_the_sem.c
@@ -6,7 +6,7 @@
 /*   By: sodahani <sodahani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:34:41 by sodahani          #+#    #+#             */
-/*   Updated: 2025/02/14 18:06:27 by sodahani         ###   ########.fr       */
+/*   Updated: 2025/02/14 18:17:23 by sodahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,13 +78,11 @@ static int	check_meals(t_philo *philo)
 	if (philo->data->m->__align == philo->data->num_philos)
 	{
 		sem_wait(philo->data->print_sem);
-		sem_wait(philo->data->death_sem);
 		if (philo->data->d->__align != 1)
 		{
 			philo->data->d->__align = 1;
 			printf("all philosophers have finished their meals\n");
 		}
-		sem_post(philo->data->death_sem);
 		sem_post(philo->data->print_sem);
 		sem_post(philo->data->meal_sem);
 		return (1);
